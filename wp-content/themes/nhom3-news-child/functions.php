54b0474c7ee7f4c4f10411397c3590bcae0d43bf
maingan06 <maingan2006thptahhnd@gmail.com>
2026-09-06 22:05:14 +0700

    function them_js_dong_thongbao
    Added JavaScript to handle notification dismissal on click.

--- wp-content/themes/nhom3-news-child/functions.php
+++ wp-content/themes/nhom3-news-child/functions.php
@@ -2,3 +2,16 @@
 function nhom3_secure_shortcode($atts) { 
     $clean_text = sanitize_text_field( isset($atts['text']) ? $atts['text'] : 'Chao mung den voi Nhom 3' ); 
 } 
+// Bạn 1 hoặc Bạn 4 thêm đoạn này vào functions.php để nhúng JS
+function them_js_dong_thongbao() {
+    ?>
+    <script>
+    document.addEventListener('click', function(e) {
+        if (e.target.classList.contains('thongbao-dong')) {
+            e.target.closest('.thongbao').style.display = 'none';
+        }
+    });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'them_js_dong_thongbao');
